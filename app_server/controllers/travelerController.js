2afe56b82676d887d67df57b728041bd7a6d65b2
Jael Ortiz <jael.medel30@gmail.com>
2026-03-17 14:08:05 -0500

    Module 3 complete - MVC dyanmic rendering

--- app_server/controllers/travelerController.js
+++ app_server/controllers/travelerController.js
@@ -1,9 +1,22 @@
+const trips = require('../data/trips.json');
+
+// Home Page
 const renderHomePage = (req, res) => {
-    res.render('index', {
-        title: 'Travlr Getaways'
-    });
+  res.render('index', {
+    title: 'Travlr Getaways',
+    trips: trips
+  });
+};
+
+// Travel Page
+const renderTravelPage = (req, res) => {
+  res.render('travel', {
+    title: 'Travel',
+    trips: trips
+  });
 };
 
 module.exports = {
-    renderHomePage
+  renderHomePage,
+  renderTravelPage
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const travelerController = require('../controllers/travelerController');
 
 router.get('/', travelerController.renderHomePage);
+router.get('/travel', travelerController.renderTravelPage);
 
 module.exports = router;
